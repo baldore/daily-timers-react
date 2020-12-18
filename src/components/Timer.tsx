5d0778353d97a7d4556e4a21963b42dff9b799e0
baldore <acornssn@gmail.com>
2020-12-18 11:56:49 -0500

    Add restartTimer in useTimer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,40 +1,44 @@
-import { useEffect, useState } from 'react'
-
-const time = 10 // 10 seconds
+import { useEffect, useRef, useState } from 'react'
 
 function useTimer(initialValue: number) {
   const [count, setCount] = useState(initialValue)
   const [isDone, setIsDone] = useState(false)
+  const loop = useRef<number>()
+
+  const timerFn = () => {
+    if (isDone) return
+
+    const newCount = count - 1
+    setCount(newCount)
+    if (newCount === 0) {
+      setIsDone(true)
+      clearInterval(loop.current)
+    }
+  }
+
+  const restartTimer = () => {
+    clearInterval(loop.current)
+    setCount(initialValue)
+    setIsDone(false)
+    loop.current = setInterval(timerFn, 1000)
+  }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (isDone) return
-
-      const newCount = count - 1
-      if (newCount > 0) {
-        setCount(newCount)
-      } else {
-        setCount(0)
-        setIsDone(true)
-        clearInterval(interval)
-      }
-    }, 1000)
-
-    return () => {
-      clearInterval(interval)
-    }
+    loop.current = setInterval(timerFn, 1000)
+    return () => clearInterval(loop.current)
   }, [count])
 
-  return { count, isDone }
+  return { count, isDone, restartTimer }
 }
 
 function Timer() {
-  const { count, isDone } = useTimer(10)
+  const { count, isDone, restartTimer } = useTimer(3)
 
   return (
     <div>
       <div>{count}</div>
       {isDone && <h2>Done!</h2>}
+      <button onClick={restartTimer}>Restart?</button>
     </div>
   )
 }
