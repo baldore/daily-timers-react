8629035c041ac5df34591c42680734e96439c512
baldore <acornssn@gmail.com>
2020-12-21 14:16:33 -0500

    Playing with notifications API

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,36 @@
+import { useEffect } from 'react'
 import useTimer from '../hooks/useTimer'
 
+const promptAudio = '/assets/audio/prompt.mp3'
+
+function notify() {
+  if (Notification.permission !== 'granted') {
+    return
+  }
+
+  const notification = new Notification('Finished timer: <label>', {
+    body: 'ujuuuu',
+    actions: [],
+  })
+  notification.onclick = function (e) {
+    e.preventDefault()
+    window.focus()
+  }
+
+  const audio = new Audio(promptAudio)
+  audio.play()
+}
+
 function Timer(): JSX.Element {
-  const { count, isDone, restartTimer } = useTimer(3)
+  const { count, isDone, restartTimer } = useTimer({
+    initialValue: 3,
+  })
+
+  useEffect(() => {
+    if (isDone) {
+      notify()
+    }
+  }, [isDone])
 
   return (
     <div>
